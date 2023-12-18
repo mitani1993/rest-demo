b9c6452a2e4a2536b3684ed7ba7834ebe15babd3
mitani1993 <69582233+mitani1993@users.noreply.github.com>
2023-12-18 21:49:33 +0900

    post

--- src/main/java/com/example/restdemo/Service/ItemService.java
+++ src/main/java/com/example/restdemo/Service/ItemService.java
@@ -1,5 +1,6 @@
 package com.example.restdemo.Service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -10,12 +11,12 @@ import com.example.restdemo.model.Item;
 @Service
 public class ItemService {
 
-    private List<Item> allItems = Arrays.asList(
+    private List<Item> allItems = new ArrayList<>(Arrays.asList(
             new Item("10001", "ネックレス", "ジュエリ"),
             new Item("10002", "パーカー", "ファッション"),
             new Item("10003", "フェイスクリーム", "ビューティー"),
             new Item("10004", "サプリメント", "ヘルス"),
-            new Item("10005", "ブルーベリー", "フード"));
+            new Item("10005", "ブルーベリー", "フード")));
 
     public List<Item> getAItems() {
         return allItems;
@@ -27,5 +28,9 @@ public class ItemService {
                 return item;
         }
         return null;
+    }
+
+    public void addItem(Item item) {
+        allItems.add(item);
     }
 }
--- src/main/java/com/example/restdemo/controller/ItemController.java
+++ src/main/java/com/example/restdemo/controller/ItemController.java
@@ -10,6 +10,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 public class ItemController {
@@ -25,6 +27,11 @@ public class ItemController {
     @GetMapping("/items/{itemId}")
     public Item getItem(@PathVariable String itemId) {
         return itemService.getItem(itemId);
+    }
+
+    @PostMapping("/items")
+    public void addItem(@RequestBody Item item) {
+        itemService.addItem(item);
     }
 
 }
