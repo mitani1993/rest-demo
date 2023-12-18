f7d767cecf95f372a21b800c13c9a142f09b66b6
mitani1993 <69582233+mitani1993@users.noreply.github.com>
2023-12-18 22:16:50 +0900

    update

--- src/main/java/com/example/restdemo/controller/ItemController.java
+++ src/main/java/com/example/restdemo/controller/ItemController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
 
 @RestController
 public class ItemController {
@@ -34,4 +35,8 @@ public class ItemController {
         itemService.addItem(item);
     }
 
+    @PutMapping("items/{itemId}")
+    public void updateItem(@PathVariable String itemId, @RequestBody Item item) {
+        itemService.updateItem(itemId, item);
+    }
 }
